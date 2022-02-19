2f416686787bc2d0ec91fae5bf9ed646276fd146
Vini Santos <vini_santos7@live.com>
2022-02-19 18:52:23 -0300

    new logo, adjust index and stylesheets e new feature on button

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,16 @@ function toggleDarkMode() {
   body.className = currentClass == "light" ? "dark" : "light"
 }
 
+const toTop = document.querySelector(".scrollTop")
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset < 100) {
+    toTop.classList.add("active")
+  } else {
+    toTop.classList.remove("active")
+  }
+})
+
 function scrollToTop() {
   window.scrollTo({
     top: 0,
